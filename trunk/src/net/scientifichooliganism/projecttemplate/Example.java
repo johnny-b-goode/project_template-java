7e1e03c6b295f4f92681efa80b099e16cd309eec
johntatum <johntatum@105c1084-4431-4a58-a0e2-519d66a80a81>
2017-07-18 04:51:04 +0000

    Created TTB. Added initial project contents.

---
+++ trunk/src/net/scientifichooliganism/projecttemplate/Example.java
@@ -0,0 +1,5 @@
+package net.scientifichooliganism.projecttemplate;
+
+public class Example {
+	//
+}
---
+++ trunk/tst/net/scientifichooliganism/projecttemplate/ExampleTest.java
@@ -0,0 +1,12 @@
+package net.scientifichooliganism.projecttemplate;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.runner.*;
+
+public class ExampleTest {
+	@Test
+	public void test01 () {
+		Assert.assertTrue(true);
+	}
+}
